b6293c5829db1257e3aae630f1db19332ed19978
Connor Ring <connorring7778@sjccs142.saintjoe.edu>
2015-03-30 15:43:33 -0400

    Unfinished but working code

--- date.js
+++ date.js
@@ -1,9 +1,18 @@
+//Connor Ring
+//Date Problem
+
+// Create the dates
 var today;
 var yesterday;
 var today = new Date().getTime();
-var yesterday = new Date(2015, 2, 30).getTime();
+var yesterday = new Date(2015, 2, 29).getTime();
 
-console.log(today);
-console.log(yesterday);
-
-console.log(today >  yesterday);
+// Print the dates out, then print out the one that is older
+console.log(new Date(today));
+console.log(new Date(yesterday));
+console.log(today > yesterday);
+//Primitive if else code...Can't get it to work right
+//if today(value)  > yesterday(value) {
+//console.log(new Date(today));
+//}
+//else console.log(new Date(yesterday));
