45ed960e53a5d037ab1603d4dc0d7880ed725009
Connor Ring <connorring7778@sjccs142.saintjoe.edu>
2015-03-30 14:58:45 -0400

    Finished array.js problem

--- array.js
+++ array.js
@@ -2,11 +2,11 @@
 //Problem 3
 
 // Display array
-var ARRAY = [
-{Name: "Connor", Height: "71", Weight: "200"},
-{Name: "Jacob", Height: "68", Weight: "170"},
-{Name: "Taylor", Height: "79", Weight: "220"},
-];
+var ARRAY = "[\n " +  [
+ '{"Name" : "Connor", "Height" : "71", "Weight" : "200"}',
+ '{"Name" : "Jacob", "Height" : "68", "Weight" : "170"}',
+ '{"Name" : "Taylor", "Height" : "79", "Weight" : "220"}'
+].join(",\n  ") + "\n]";
 
 if (typeof module != "undefined" && module.exports)
   module.exports = ARRAY;
